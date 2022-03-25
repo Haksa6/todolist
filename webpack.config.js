b59eeca2014549bf88d6444d8228b957dd1bfd25
Haksa6 <77583826+Haksa6@users.noreply.github.com>
2022-03-25 19:24:38 +0200

    start

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", //3. Inject styles into DOM
+          "css-loader", //2. Turns css into js
+          "sass-loader" //1. Turns sass into css
+      ]
+      }
+    ]
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+};
