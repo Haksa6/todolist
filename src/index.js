dc40e7dd1e92395668b6e2e60ff78a75a828a957
Haksa6 <77583826+Haksa6@users.noreply.github.com>
2022-05-29 12:19:19 +0300

    frontend and popscreen to create new finished

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './sass/main.scss';
+const getTodos = () => {
+  let todos;
+  if(localStorage.getItem("todos") === null){
+    todos = [];
+  }else{
+    JSON.parse(localStorage.getItem("todos"));
+  }
+  todos.forEach
+}
